2065a552b62513f3e2e6e72f4e2963b684a414bc
saqibbedar <chandio.saqib.m@gmail.com>
2024-07-27 06:29:29 -0700

    added footer component

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import "./Footer.css"
+import {assets} from "../../assets/assets"
+
+const Footer = () => {
+    
+  return (
+    <div className="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+            <img src={assets.logo} />
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque nulla hic vitae aperiam blanditiis repudiandae illo, facere, tenetur quos fugiat nostrum eius molestiae. Veniam voluptatibus, tenetur minima ipsum ipsam placeat?</p>
+            <div className="footer-social-icons">
+                <img src={assets.facebook_icon}/>
+                <img src={assets.twitter_icon}/>
+                <img src={assets.linkedin_icon}/>
+            </div>
+        </div>
+        <div className="footer-content-center">
+            <h2>COMPANY</h2>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy Policy</li>
+            </ul>
+        </div>
+        <div className="footer-content-right">
+            <h2>GET IN TOUCH</h2>
+            <ul>
+                <li>+92-305-3721987</li>
+                <li>contact@tomato.com</li>
+            </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 © Tomato.com - All Right Reserved.
+      </p>
+    </div>
+  )
+}
+
+export default Footer
