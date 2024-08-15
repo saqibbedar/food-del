3e5e26443aa2cac070c2e74f18b50899f23c681b
saqibbedar <Chandio.Saqib.M@gmail.com>
2024-08-16 02:41:58 +0500

    added logic for addFood endpoint

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -3,6 +3,23 @@ import fs from "fs";
 
 // add food item
 
-const addFood = async (req, res) => {};
+const addFood = async (req, res) => {
+  let image_filename = `${req.file.filename}`;
+  const food = new foodModel({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    image: image_filename,
+    category: req.body.category,
+  });
+
+  try {
+    await food.save();
+    res.json({ success: true, message: "Food Added" });
+  } catch (e) {
+    console.log(e);
+    res.json({ success: false, message: "Error while saving food item." });
+  }
+};
 
 export { addFood };
