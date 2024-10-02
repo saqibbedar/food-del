67f271db4f6df57d23464b19c1b53b44c1dfd3d8
saqibbedar <chandio.saqib.m@gmail.com>
2024-10-02 21:28:20 +0500

    feat(routes): add user routes for login and register

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from "dotenv";
 import { connectToDataBase } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ connectToDataBase(`${process.env.MONGODB_URL}/food-del`);
 // api endpoints
 app.use("/api/food", foodRouter);
 app.use('/images', express.static('uploads'))
+app.use("/api/user", userRouter);
 
 app.get('/', (req, res)=>{
     res.send("Server is working!");
