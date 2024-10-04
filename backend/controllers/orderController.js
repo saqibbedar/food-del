add720be77c73ae57c3d8aa3240a3c1fec244990
saqibbedar <chandio.saqib.m@gmail.com>
2024-10-05 01:14:00 +0500

    feat(orderAndPayments): add orderModel, orderController, orderRouter and integrate payment method using Stripe

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,72 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+import Stripe from "stripe";
+import "dotenv/config";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+// placing user order for frontend
+const placeOrder = async (req, res) => {
+  try {
+    const newOrder = new orderModel({
+      userId: req.body.userId,
+      items: req.body.items,
+      amount: req.body.amount,
+      address: req.body.address,
+    });
+    await newOrder.save();
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
+
+    const line_items = req.body.items.map((item) => ({
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.quantity,
+    }));
+
+    line_items.push({
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: "Delivery Charges",
+        },
+        unit_amount: 2,
+      },
+      quantity: 1,
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      line_items: line_items,
+      mode: "payment",
+      success_url: `${process.env.FRONTEND_URL}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${process.env.FRONTEND_URL}/verify?success=false&orderId=${newOrder._id}`,
+    });
+
+    res.json({ success: true, session_url: session.url });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: "Error while placing order" });
+  }
+};
+
+const verifyOrder = async (req, res) => {
+  const { orderId, success } = req.body;
+  try {
+    if (success === "true") {
+      await orderModel.findByIdAndUpdate(orderId, { payment: true });
+      res.json({ success: true, message: "Paid" });
+    } else {
+      await orderModel.findByIdAndDelete(orderId);
+      res.json({ success: false, message: "Not Paid" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: "Error while verifying payments" });
+  }
+};
+
+export { placeOrder, verifyOrder };
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { connectToDataBase } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
 import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 // dotenv config
 dotenv.config();
@@ -25,6 +26,7 @@ app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("Server is working!");
