f616a6dbb667c326b1d441206e5f7ff8749b1af4
saqibbedar <chandio.saqib.m@gmail.com>
2024-10-05 03:05:09 +0500

    feat(frontend): add MyOrders page, fetch userorders and protect routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import Login from "./components/Login/Login";
 import { ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Verify from "./pages/Verify/Verify";
+import MyOrders from "./pages/MyOrders/MyOrders";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false)
@@ -23,6 +24,7 @@ const App = () => {
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
           <Route path="/verify" element={<Verify />} />
+          <Route path="/myorders" element={<MyOrders/>} />
         </Routes>
       </div>
       <Footer />
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,17 +1,28 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import {useNavigate} from "react-router-dom"
 import './Cart.css'
 import { StoreContext } from "../../context/StoreContext"
 import {url} from "../../assets/assets.js"
 
 const Cart = () => {
-
+    const [isButtonDisable, setIsButtonDisable] = useState(true);
   const navigate = useNavigate();
 
-  const {cartItems, food_list, removeFromCart, getTotalCartAmount} = useContext(StoreContext)
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount, token } = useContext(StoreContext);
+
+  useEffect(() => {
+    if (!token) {
+      setIsButtonDisable(true);
+    } else if (getTotalCartAmount() <= 0) {
+      console.log(getTotalCartAmount);
+      setIsButtonDisable(true);
+    } else {
+      setIsButtonDisable(false);
+    }
+  }, [token, getTotalCartAmount]);
 
   return (
-    <div className='cart-container'>
+    <div className="cart-container">
       <div className="cart-items">
         <div className="cart-items-title">
           <p>Items</p>
@@ -23,25 +34,25 @@ const Cart = () => {
         </div>
         <br />
         <hr />
-        {
-          food_list.map((item, index)=>{
-            if(cartItems[item._id]>0){
-              return(
-                <div className='cart-items-holder'>
-                  <div className="cart-items-title cart-items-item">
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div className="cart-items-holder">
+                <div className="cart-items-title cart-items-item">
                   <img src={`${url}/images/${item.image}`} />
                   <p>{item.name}</p>
                   <p>${item.price}</p>
                   <p>{cartItems[item._id]}</p>
                   <p>${item.price * cartItems[item._id]}</p>
-                  <p className='cross' onClick={()=>removeFromCart(item._id)}>x</p>
+                  <p className="cross" onClick={() => removeFromCart(item._id)}>
+                    x
+                  </p>
                 </div>
                 <hr />
-                </div>
-              )
-            }
-          })
-        }
+              </div>
+            );
+          }
+        })}
       </div>
       <div className="cart-bottom">
         <div className="cart-total">
@@ -53,27 +64,33 @@ const Cart = () => {
           <hr />
           <div className="cart-total-details">
             <p>Delivery Fee</p>
-            <p>${getTotalCartAmount()=== 0? 0: 2}</p>
+            <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
           </div>
           <hr />
           <div className="cart-total-details">
             <b>Total</b>
-            <b>${getTotalCartAmount() === 0 ? 0: getTotalCartAmount() + 2}</b>
+            <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
           </div>
-          <button onClick={()=>navigate("/order")}>PROCEED TO CHECKOUT</button>
+          <button
+            disabled={isButtonDisable}
+            style={{ background: isButtonDisable ? "#a1a1a1" : "tomato", cursor: isButtonDisable? "no-drop" : "pointer" }}
+            onClick={() => navigate("/order")}
+          >
+            PROCEED TO CHECKOUT
+          </button>
         </div>
         <div className="cart-promocode">
           <div>
             <p>If you have a promo code, Enter it here</p>
             <div className="cart-promocode-input">
-              <input type="text" placeholder='promo code'/>
+              <input type="text" placeholder="promo code" />
               <button>Submit</button>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Cart
