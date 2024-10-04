8acccaeb12201dfb951fc1525bd0e0dc7d609028
saqibbedar <chandio.saqib.m@gmail.com>
2024-10-04 12:22:39 +0500

    feat(cartData): add auth middleware, cartController, cartRoute and setup api endpoint to manage cartData

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,53 @@
+import userModel from "../models/userModel.js";
+
+// add items to user cart
+const addToCart = async (req, res) => {
+  try {
+    // we are getting userId from auth middleware
+    const userData = await userModel.findById(req.body.userId);
+    const cartData = await userData.cartData;
+    if (!cartData[req.body.itemId]) {
+      cartData[req.body.itemId] = 1;
+    } else {
+      cartData[req.body.itemId] += 1;
+    }
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    res.json({ success: true, message: "Added To Cart" });
+  } catch (error) {
+    console.error(error);
+    res.json({
+      success: false,
+      message: "Error while saving items to the Cart",
+    });
+  }
+};
+
+// remove items from user cart
+const removeFromCart = async (req, res) => {
+  try {
+    const userData = await userModel.findById(req.body.userId);
+    const cartData = userData.cartData;
+    if (cartData[req.body.itemId] > 0) {
+      cartData[req.body.itemId] -= 1;
+    } 
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    res.json({success: true, message: "Removed From Cart"})
+  } catch (error) {
+    console.error(error);
+    res.json({success: false, message: "Error while removing data from Cart"})
+  }
+};
+
+// fetch user cart data
+const getCart = async (req, res) => {
+  try {
+    const userData = await userModel.findById(req.body.userId);
+    const cartData = await userData.cartData;
+    res.json({ success: true, cartData });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: "Error while getting cart data" });
+  }
+};
+
+export { addToCart, removeFromCart, getCart };
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
 import dotenv from "dotenv";
 import { connectToDataBase } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
+import cartRouter from "./routes/cartRoute.js";
 
+// dotenv config
 dotenv.config();
 
 // app config
@@ -20,11 +22,14 @@ connectToDataBase(`${process.env.MONGODB_URL}/food-del`);
 
 // api endpoints
 app.use("/api/food", foodRouter);
-app.use('/images', express.static('uploads'))
+app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
 
-app.get('/', (req, res)=>{
-    res.send("Server is working!");
-})
+app.get("/", (req, res) => {
+  res.send("Server is working!");
+});
 
-app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server started on http://localhost:${PORT}`)
+);
