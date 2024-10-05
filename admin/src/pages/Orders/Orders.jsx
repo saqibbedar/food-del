d0dc546195087ada2b2b0cd13126e4f76e172602
saqibbedar <chandio.saqib.m@gmail.com>
2024-10-05 11:50:03 +0500

    feat(admin): integrate food order list and add styles

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -1,11 +1,68 @@
-import './Orders.css'
+import { assets, url } from '../../assets/assets';
+import './Orders.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Orders = () => {
+  const [data, setData] = useState([]);
+  const fetchAllOrders = async () => {
+    const response = await axios.get(`${url}/api/order/list`);
+    if (response.data.success) {
+      setData(response.data.data);
+    } else {
+      toast.error(response.data.message);
+    }
+  }
+  useEffect(() => {
+    fetchAllOrders();
+  }, [])
   return (
-    <div>
-      
+    <div className="order add">
+      <h3>Order Page</h3>
+      <div className="order-list">
+        {data.map((order, index) => (
+          <div key={index} className="order-item">
+            <img src={assets.parcel_icon} alt="" />
+            <div>
+              <p className="order-item-food">
+                {order.items.map((item, index) => {
+                  if (index === order.items.length - 1) {
+                    return item.name + " x " + item.quantity;
+                  } else {
+                    return item.name + " x " + item.quantity + ", ";
+                  }
+                })}
+              </p>
+              <p className="order-item-name">
+                {order.address.firstName + " " + order.address.lastName}
+              </p>
+              <div className="order-item-address">
+                <p>{order.address.street + ", "}</p>
+                <p>
+                  {order.address.city +
+                    ", " +
+                    order.address.street +
+                    ", " +
+                    order.address.country +
+                    ", " +
+                    order.address.zipCode}
+                </p>
+              </div>
+              <p className="order-item-phone">{order.address.phone}</p>
+            </div>
+            <p>Items: {order.items.length}</p>
+            <p>${order.amount}</p>
+            <select>
+              <option value="Food Processing">Food Processing</option>
+              <option value="Out for Delivery">Out for Delivery</option>
+              <option value="Delivered">Delivered</option>
+            </select>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Orders
