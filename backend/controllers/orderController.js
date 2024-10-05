01d67c11375d0def97a465dcca3184069c3d8134
saqibbedar <chandio.saqib.m@gmail.com>
2024-10-05 11:52:55 +0500

    feat(OrdersAPI): add api to load all food orders for admin panel

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -80,4 +80,16 @@ const userOrders = async (req, res) => {
   }
 }
 
-export { placeOrder, verifyOrder, userOrders };
+// listing orders for admin panel
+
+const listOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: "Error while fetching user order list" });
+  }
+}
+
+export { placeOrder, verifyOrder, userOrders, listOrders };
